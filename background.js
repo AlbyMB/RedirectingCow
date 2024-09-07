6f26b0e861d52683f2ddbf0c40cc7da51742baf8
Alby Mathews Bruse <albymbruse@gmail.com>
2024-09-07 13:24:11 -0400

    Added Functionality

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-      // Check if the request is for YouTube
-      if (details.url.includes("youtube.com")) {
-        return { redirectUrl: "https://leetcode.com/problemset/all/" };
+      if (localStorage.getItem(details.url)) {
+        return { redirectUrl: `${localStorage.getItem(details.url)}`};
       }
     },
-    { urls: ["*://*.youtube.com/*"], types: ["main_frame"] },
+    { urls: ["<all_urls>"]},
     ["blocking"]
   );
--- popup.js
+++ popup.js
@@ -3,5 +3,16 @@ window.onload = (async () => {
     const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
     console.log(tab.url);
     document.getElementById("redirectInput").value = tab.url;
+
+    const addRedirectButton = document.getElementById("addRedirectButton");
+
+    addRedirectButton.addEventListener('click', AddRedirection);
     
 })();
+
+const AddRedirection = () => {
+    const redirectFrom = document.getElementById("redirectInput").value;
+    const redirectTo = document.getElementById("toInput").value;
+    localStorage.setItem(`${redirectFrom}`, `${redirectTo}`);
+
+}
